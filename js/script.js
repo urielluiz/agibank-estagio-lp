13728a141ed6acf503798a33bed9398d3faaceac
urielluiz <urielluizsantos@gmail.com>
2026-09-22 10:37:01 -0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/* ============================================
+   AGIBANK - PROGRAMA DE ESTÁGIO LP
+   JavaScript principal
+============================================ */
+
+document.addEventListener('DOMContentLoaded', function () {
+  console.log('Agibank LP - Estágio carregada com sucesso ✅');
+
+  // Aqui entrarão futuramente:
+  // - Menu mobile (toggle)
+  // - Scroll animations
+  // - Interações de micro animação
+  // - Validação de formulário (se houver)
+});
